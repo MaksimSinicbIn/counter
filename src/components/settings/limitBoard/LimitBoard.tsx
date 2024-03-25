f66a6bdd153ce5a4512773198281d61fbad6affb
MaksimSinicbIn <dzigen1986@gmail.com>
2024-03-25 13:19:42 +0700

    Add className props

--- src/components/settings/limitBoard/LimitBoard.tsx
+++ src/components/settings/limitBoard/LimitBoard.tsx
@@ -1,19 +1,20 @@
 import * as React from 'react';
-import { ChangeEvent } from 'react';
+import { ChangeEvent, DetailedHTMLProps, InputHTMLAttributes } from 'react';
 import s from './LimitBoard.module.css'
 
 type LimitBoardPropsType = {
     title: string
     value: number
+    className?: string
     onChange: (e: ChangeEvent<HTMLInputElement>) => void
-};
+}
 
-export const LimitBoard = ({title, value, onChange}: LimitBoardPropsType) => {
+export const LimitBoard = ({title, value, className, onChange}: LimitBoardPropsType) => {
     return (
         <form className={s.form}>
             <label>{title}</label>
             <input 
-                className={s.input}
+                className={className}
                 value={value}
                 type='number'
                 onChange={onChange}
