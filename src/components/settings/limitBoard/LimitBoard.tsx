83a5b8752c031c41aea223895f201a4f36ee11a2
MaksimSinicbIn <dzigen1986@gmail.com>
2024-03-26 22:55:41 +0700

    Rename props and correct styles

--- src/components/settings/limitBoard/LimitBoard.tsx
+++ src/components/settings/limitBoard/LimitBoard.tsx
@@ -1,20 +1,20 @@
 import * as React from 'react';
-import { ChangeEvent, DetailedHTMLProps, InputHTMLAttributes } from 'react';
+import { ChangeEvent } from 'react';
 import s from './LimitBoard.module.css'
 
 type LimitBoardPropsType = {
     title: string
     value: number
-    className?: string
     onChange: (e: ChangeEvent<HTMLInputElement>) => void
+    inputError: boolean
 }
 
-export const LimitBoard = ({title, value, className, onChange}: LimitBoardPropsType) => {
+export const LimitBoard = ({title, value, inputError, onChange}: LimitBoardPropsType) => {
     return (
         <form className={s.form}>
             <label>{title}</label>
             <input 
-                className={className}
+                className={inputError ? `${s.inputError} ${s.input}` : s.input}
                 value={value}
                 type='number'
                 onChange={onChange}
