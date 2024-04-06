5ef0b1e08fae2fb01c209cf19a7bd524e06c3993
MaksimSinicbIn <dzigen1986@gmail.com>
2024-04-06 15:42:35 +0700

    Refactoring states work with localStorage

--- src/App.tsx
+++ src/App.tsx
@@ -8,40 +8,45 @@ export const counterMessages = {
     error: 'Incorrect value!'
 } as const
 
+const DEFAULT_START_VALUE = 0
+const DEFAULT_MAX_VALUE = 5
+
 function App() {
 
-    const loadedMaxValue = localStorage.getItem("maxValue")
-    ? JSON.parse(localStorage.getItem("maxValue") as string)
-    : []; 
-
-    const loadedStartValue = localStorage.getItem("startValue")
-    ? JSON.parse(localStorage.getItem("startValue") as string) 
-    : [];
-
-    const [maxValue, setMaxValue] = useState<number>(loadedMaxValue)
-    const [startValue, setStartValue] = useState<number>(loadedStartValue)
-    const [count, setCount] = useState<number>(startValue)
+    const [maxValue, setMaxValue] = useState<number>(() => {
+        const loadedMaxValue = localStorage.getItem("maxValue")
+        return loadedMaxValue ? JSON.parse(loadedMaxValue) : DEFAULT_MAX_VALUE
+    })
+    const [startValue, setStartValue] = useState<number>( () => {
+        const loadedStartValue = localStorage.getItem("startValue")
+        return loadedStartValue ? JSON.parse(loadedStartValue) : DEFAULT_START_VALUE;
+    })
+    const [count, setCount] = useState<number>(() => {
+        const start = localStorage.getItem("startValue")
+        // if (start) {
+        //     return JSON.parse(start)
+        // } 
+        // ситуативная проверка, раскомментить по желанию
+        let currentCount = localStorage.getItem('countValue')
+        if (currentCount) {
+            return JSON.parse(currentCount)
+        }
+        return DEFAULT_START_VALUE
+    })
     const [error, setError] = useState<string>('')
 
     useEffect( () => {
-        let currentCount = localStorage.getItem('countValue')
-            if (currentCount) {
-                setCount(JSON.parse(currentCount))
-            }
-    }, [] );
-
-    useEffect( () => {
         localStorage.setItem('countValue', JSON.stringify(count))
-        localStorage.setItem('maxValue', JSON.stringify(maxValue))
-        localStorage.setItem('startValue', JSON.stringify(startValue))
     }, [count, maxValue, startValue] );
 
     const addMaxValue = (maxValue: number) => {
         setMaxValue(maxValue)
+        localStorage.setItem('maxValue', JSON.stringify(maxValue))
     };
 
     const addStartValue = (startValue: number) => {
         setStartValue(startValue)
+        localStorage.setItem('startValue', JSON.stringify(startValue))
     };
 
     return (
