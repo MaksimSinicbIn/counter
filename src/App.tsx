62875d0c528cdc64dac9993c6b9d164aaff06492
MaksimSinicbIn <dzigen1986@gmail.com>
2024-04-09 01:01:50 +0700

    Refactoring App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,71 +1,12 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import './App.css';
-import { Counter } from './components/counter/Counter';
-import { Settings } from './components/settings/Settings';
+import { CombinedCounter } from './components/CombinedCounter';
 
-export const counterMessages = {
-    confirm: 'Press \'Confirm\' to continue',
-    error: 'Incorrect value!'
-} as const
-
-const DEFAULT_START_VALUE = 0
-const DEFAULT_MAX_VALUE = 5
 
 function App() {
-
-    const [maxValue, setMaxValue] = useState<number>(() => {
-        const loadedMaxValue = localStorage.getItem("maxValue")
-        return loadedMaxValue ? JSON.parse(loadedMaxValue) : DEFAULT_MAX_VALUE
-    })
-    const [startValue, setStartValue] = useState<number>( () => {
-        const loadedStartValue = localStorage.getItem("startValue")
-        return loadedStartValue ? JSON.parse(loadedStartValue) : DEFAULT_START_VALUE;
-    })
-    const [count, setCount] = useState<number>(() => {
-        // const start = localStorage.getItem("startValue")
-        // if (start) {
-        //     return JSON.parse(start)
-        // } 
-        // ситуативная проверка, раскомментить по желанию
-        let currentCount = localStorage.getItem('countValue')
-        if (currentCount) {
-            return JSON.parse(currentCount)
-        }
-        return DEFAULT_START_VALUE
-    })
-    const [error, setError] = useState<string>('')
-
-    useEffect( () => {
-        localStorage.setItem('countValue', JSON.stringify(count))
-    }, [count, maxValue, startValue] );
-
-    const addMaxValue = (maxValue: number) => {
-        setMaxValue(maxValue)
-        localStorage.setItem('maxValue', JSON.stringify(maxValue))
-    };
-
-    const addStartValue = (startValue: number) => {
-        setStartValue(startValue)
-        localStorage.setItem('startValue', JSON.stringify(startValue))
-    };
-
     return (
         <div className='App'>
-            <Settings
-                setError={setError}
-                startValue={startValue}
-                maxValue={maxValue}
-                // setCount={setCount}
-                // addMaxValue={addMaxValue}
-                // addStartValue={addStartValue}
-            />
-            <Counter
-                error={error}
-                currentValue={count}
-                startValue={startValue}
-                maxValue={maxValue}
-                // setCount={setCount}
-            />
+            <CombinedCounter />
         </div>
     );
 };
