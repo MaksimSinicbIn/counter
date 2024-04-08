0a0fcbcba7c5f00ac134e1f83dc163ffab5906c5
MaksimSinicbIn <dzigen1986@gmail.com>
2024-04-08 20:06:29 +0700

    Create "container" component for counter

---
+++ src/components/FullCounter.tsx
@@ -0,0 +1,36 @@
+import * as React from 'react';
+import s from './FullCounter.module.css'
+import { Settings } from './settings/Settings';
+import { Counter } from './counter/Counter';
+import { useState } from 'react';
+import { useSelector } from 'react-redux';
+import { AppRootStateType } from './redux/store';
+import { CounterType } from './redux/counterReducer';
+
+export const counterMessages = {
+    confirm: 'Press \'Confirm\' to continue',
+    error: 'Incorrect value!'
+} as const
+
+export const FullCounter = () => {
+
+    const [error, setError] = useState<string>('')
+
+    const counter = useSelector<AppRootStateType, CounterType>( state => state.counter)
+
+    return (
+        <div className={s.fullCounter}>
+            <Settings
+                setError={setError}
+                startValue={counter.startValue}
+                maxValue={counter.maxValue}
+            />
+            <Counter
+                error={error}
+                currentValue={counter.currentValue}
+                startValue={counter.startValue}
+                maxValue={counter.maxValue}
+            />
+        </div>
+    );
+};
