7e12b09ec7f409ffc7a4de9727de6515823427b3
MaksimSinicbIn <dzigen1986@gmail.com>
2024-04-08 20:07:10 +0700

    Create counterReducer

---
+++ src/components/redux/counterReducer.ts
@@ -0,0 +1,46 @@
+type SetSettingsActionType = ReturnType<typeof setSettingsAC>
+type IncrementCounterActionType = ReturnType<typeof incrementCounterAC>
+type ResetCounterActionType = ReturnType<typeof resetCounterAC>
+
+export type ActionsType =
+    | SetSettingsActionType
+    | IncrementCounterActionType
+    | ResetCounterActionType
+
+export type CounterType = {
+    maxValue: number
+    startValue: number
+    currentValue: number
+}
+
+const initialState: CounterType = {
+    maxValue: 5,
+    startValue: 0,
+    currentValue: 0
+}
+
+export const counterReducer = (state = initialState, action: ActionsType): CounterType => {
+    switch (action.type) {
+        case 'SET-SETTINGS':
+            return {...state, maxValue: action.maxValue, startValue: action.startValue, currentValue: action.startValue}
+        case 'INCREMENT-COUNTER':
+            //localStorage.setItem
+            return {...state, currentValue: state.currentValue + 1}
+        case 'RESET-COUNTER':
+            return {...state, currentValue: state.startValue}
+        default:
+            return state
+    }
+}
+
+export const setSettingsAC = (maxValue: number, startValue: number) => {
+    return {type: 'SET-SETTINGS', maxValue, startValue} as const
+}
+
+export const incrementCounterAC = () => {
+    return { type: 'INCREMENT-COUNTER'}  as const
+}
+
+export const resetCounterAC = () => {
+    return { type: 'RESET-COUNTER'} as const
+}
