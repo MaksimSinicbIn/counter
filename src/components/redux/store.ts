49acb8b38aa005e3208779f88daf30f4b992dd6a
MaksimSinicbIn <dzigen1986@gmail.com>
2024-04-09 00:20:00 +0700

    Add a subscriber to download state from localStorage

--- src/components/redux/store.ts
+++ src/components/redux/store.ts
@@ -1,10 +1,20 @@
 import { combineReducers, legacy_createStore as createStore } from "redux";
 import { counterReducer } from "./counterReducer";
+import { loadState, saveState } from "../../localStorage";
+import { throttle } from "lodash";
 
 const rootReducer = combineReducers({
     counter: counterReducer
 })
 
-export const store = createStore(rootReducer)
+const persistedState = loadState();
+
+export const store = createStore(rootReducer, persistedState)
+
+store.subscribe(throttle(() => {
+    saveState({
+        ...store.getState(),
+    })
+}, 1000))
 
 export type AppRootStateType = ReturnType<typeof rootReducer>
