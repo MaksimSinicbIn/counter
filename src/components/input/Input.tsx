df0be8d104174499bd84df8e0945bba95e31ac8a
MaksimSinicbIn <dzigen1986@gmail.com>
2024-04-14 23:39:30 +0700

    Create Input component and css module

---
+++ src/components/input/Input.tsx
@@ -0,0 +1,24 @@
+import { ChangeEvent } from "react"
+import s from './Input.module.css'
+
+type InputPropsType = {
+    title: string
+    value: number
+    onChange: (e: ChangeEvent<HTMLInputElement>) => void
+    inputError: boolean
+    className?: string
+}
+
+export const Input = ({title, value, inputError, className, onChange}: InputPropsType) => {
+    return (
+        <form className={s.form}>
+            <label>{title}</label>
+            <input 
+                className={inputError ? `${s.inputError} ${s.input}` : s.input}
+                value={value}
+                type='number'
+                onChange={onChange}
+            />
+        </form>
+    );
+};
